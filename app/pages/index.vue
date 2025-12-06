a7842f9ccf001db8d593f45eed33cbd638d3881e
unknown <cmtliton@gmail.com>
2025-12-06 17:25:03 +0600

    3rd commit

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,12 +17,12 @@ useSeoMeta({
     "Become a Nuxt expert with the official Mastering Nuxt 4 course. Learn full-stack development, server-side rendering, and advanced patterns.",
   ogDescription:
     "Become a Nuxt expert with the official Mastering Nuxt 4 course. Learn full-stack development, server-side rendering, and advanced patterns.",
-  ogImage: "/public/og-images.png", // Replace with actual image URL if different
+  ogImage: "/og-images.png", // Replace with actual image URL if different
   ogUrl: "https://mastering-nuxt4.netlify.app/",
   twitterCard: "summary_large_image",
   twitterTitle: "Mastering Nuxt 4",
   twitterDescription:
     "The official course to master Nuxt 4 and modern web development.",
-  twitterImage: "https://mastering-nuxt4.netlify.app/images/og-images.png", // Replace with actual image URL if different
+  twitterImage: "https://mastering-nuxt4.netlify.app/og-images.png", // Replace with actual image URL if different
 });
 </script>
