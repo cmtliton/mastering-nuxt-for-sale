db4902fc40f2de88afce826076d6b4f42d4721cf
unknown <cmtliton@gmail.com>
2025-12-06 17:18:51 +0600

    2nd Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module"],
+  app: {
+    head: {
+      title: "Mastering Nuxt 4 Full Course - Build a Nuxt 4 App from Scratch",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content:
+            "The Official Full Course for Mastering Nuxt 4. Learn how to build a Nuxt 4 application from scratch with step-by-step guidance.",
+        },
+      ],
+    },
+  },
 });
